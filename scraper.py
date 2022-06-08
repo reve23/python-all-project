1c1eacb54907780c9b34f439a6414821e4e2b3f1
reve23 <78494847+reve23@users.noreply.github.com>
2022-06-08 12:43:22 +0600

    Delete scraper.py

--- scraper.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-page = requests.get("https://www.freecodecamp.org/news/web-scraping-python-tutorial-how-to-scrape-data-from-a-website/")
-soup = BeautifulSoup(page.content,'html.parser')
-title = soup.title
-print(title.text)
-div = soup.div
-print(div.text)
